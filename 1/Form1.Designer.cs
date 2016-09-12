ede2c5c905aff9bb5f380ade42d6987d3d14d4ee
nguyenthanhxuanngoc <nguyenthanhxuanngoc@vietnthonga.com>
2016-09-12 15:03:49 +0700

    - update file text

--- 1/Form1.Designer.cs
+++ 1/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(115, 82);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(133, 95);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(694, 348);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
